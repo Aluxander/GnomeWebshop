1373ec077f66ac19f16376e27af83c0fc3a9e480
Aluxander <alexlundh@gmail.com>
2016-05-06 16:12:09 +0100

    template for index page done!

--- build/generated-sources/ap-source-output/gnome/model/Gnome_.java
+++ build/generated-sources/ap-source-output/gnome/model/Gnome_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-05T20:22:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-05-06T14:42:33")
 @StaticMetamodel(Gnome.class)
 public class Gnome_ { 
 
